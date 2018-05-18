b50092fc935ac6b3219badb7771d8b76cc056b38
Joseph Parker <jparker010190@gmail.com>
2018-05-18 00:04:51 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,7 +6,7 @@ class Backer
   end
   def back_project(project)
     @backed_projects << project
-    project.add_backer(self)
+    project.add_backer(self) unless project.add_backers.include?(self)
   end
 
 end
